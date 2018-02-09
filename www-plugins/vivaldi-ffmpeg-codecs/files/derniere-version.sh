c02c7aa2109de7c9f733643c0748c6e49ab5f398
Adrien_D <ghbdtn@openmailbox.org>
2018-02-09 17:48:29 +0100

    vivaldi test

--- www-plugins/vivaldi-ffmpeg-codecs/files/derniere-version.sh
+++ www-plugins/vivaldi-ffmpeg-codecs/files/derniere-version.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 rm *.deb
-deb32=$(wget -O - -q http://ppa.launchpad.net/saiarcot895/chromium-beta/ubuntu/pool/main/c/chromium-browser/ | grep ffmpeg-extra | grep '16.04' | grep i386 | tail -1 | cut -d\" -f8)
-deb64=$(wget -O - -q http://ppa.launchpad.net/saiarcot895/chromium-beta/ubuntu/pool/main/c/chromium-browser/ | grep ffmpeg-extra | grep '16.04' | grep amd64 | tail -1 | cut -d\" -f8)
+deb32=$(wget -O - -q http://ppa.launchpad.net/saiarcot895/chromium-beta/ubuntu/pool/main/c/chromium-browser/ | grep ffmpeg-extra | grep '17.10' | grep i386 | tail -1 | cut -d\" -f8)
+deb64=$(wget -O - -q http://ppa.launchpad.net/saiarcot895/chromium-beta/ubuntu/pool/main/c/chromium-browser/ | grep ffmpeg-extra | grep '17.10' | grep amd64 | tail -1 | cut -d\" -f8)
 wget http://ppa.launchpad.net/saiarcot895/chromium-beta/ubuntu/pool/main/c/chromium-browser/$deb32
 wget http://ppa.launchpad.net/saiarcot895/chromium-beta/ubuntu/pool/main/c/chromium-browser/$deb64
