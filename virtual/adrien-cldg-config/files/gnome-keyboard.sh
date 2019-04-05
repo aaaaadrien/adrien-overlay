a5f526afeefce724d32b6d2c4ce8ea5cd297659d
Adrien_D <ghbdtn@openmailbox.org>
2019-04-05 21:19:09 +0200

    Add calculate wallpaper

--- virtual/adrien-cldg-config/files/gnome-keyboard.sh
+++ virtual/adrien-cldg-config/files/gnome-keyboard.sh
@@ -2,9 +2,16 @@
 
 #Set french keyboard if language FR
 L=$(echo $LANG | cut -d_ -f1)
-K=$(gesttings get org.gnome.desktop.input-sources sources | cut -d\' -f4)
+K=$(gsettings get org.gnome.desktop.input-sources sources | cut -d\' -f4)
 
 if [[ "$L" == "fr" ]] && [[ "$K" == "us" ]]
 then
-	gesttings set org.gnome.desktop.input-sources sources "[('xkb', 'fr+oss')]"
+	gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'fr+oss')]"
+fi
+
+# Background
+B=$(gsettings get org.gnome.desktop.background picture-uri)
+if [[ "$B" == "'file:///usr/share/backgrounds/gnome/adwaita-timed.xml'" ]]
+then
+	gsettings set org.gnome.desktop.background picture-uri "'file://usr/share/themes/Calculate/wallpaper.jpg'"
 fi
